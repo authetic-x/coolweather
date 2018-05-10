bf6b49127deb270a96b49db016a3d33a9730273f
authetic <1019892846@qq.com>
2018-05-10 17:09:53 +0800

    添加天气界面显示功能

---
+++ app/src/main/java/com/example/authetic/coolweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.authetic.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public AQICity city;
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
